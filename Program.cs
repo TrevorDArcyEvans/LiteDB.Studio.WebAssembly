775fd27e90af64533e7a3570edeb0763d0a0b348
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-11-04 17:13:39 +0000

    - initial MudBlazor integration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using KristofferStrube.Blazor.FileSystemAccess;
+using LiteDB.Studio.WebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using LiteDB.Studio.WebAssembly;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFileSystemAccessService();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
